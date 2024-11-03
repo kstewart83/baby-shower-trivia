247e25c3569ad239af9b84edbe66e0088019ea86
Kyle Stewart <kstewart83@gmail.com>
2024-11-03 00:51:55 +0000

    add view count

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,6 @@ export default function Home() {
         <input type="submit" value="Submit"/>
       </form>
       <Suspense>
-        {/* @ts-expect-error Async Server Component */}
         <ViewCounter />
       </Suspense>
     </main>

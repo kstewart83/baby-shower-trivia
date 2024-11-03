b092154440b65f8a8ff113e2f3347f5bdc382bd0
Kyle Stewart <kstewart83@gmail.com>
2024-11-03 00:47:08 +0000

    format landing page with kv integration

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,23 @@ export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
       <h1 className="underline">Baby Shower Trivia</h1>
+      <Image 
+        src="/baby-bassinet-cradle-bed.png" 
+        width={200}
+        height={200}
+        alt='bassinett'
+      />
+      <form>
+        <label htmlFor="first-name">First Name</label>
+        <input id="first-name" type="text"/>
+        <label htmlFor="last-name">Last Name</label>
+        <input id="last-name" type="text"/>      
+        <input type="submit" value="Submit"/>
+      </form>
+      <Suspense>
+        {/* @ts-expect-error Async Server Component */}
+        <ViewCounter />
+      </Suspense>
     </main>
   )
 }
